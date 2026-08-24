14bc48f02e31210c494d27fbb56d379dd01b6b05
Parul Singh <singhparulsingh7890@gmail.com>
2026-08-24 10:55:41 +0530

    Time: 2 ms (99.34%) | Memory: 47 MB (59.51%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,9 +4,11 @@ class Solution {
        for(int i = 0; i < nums.length; i++){
         
         int complement = target - nums[i];
+
         if(map.containsKey(complement)){
             return new int[]{i, map.get(complement) };
-        }map.put(nums[i],i);
+        }
+        map.put(nums[i],i);
        }
        return new int[]{}; 
         
