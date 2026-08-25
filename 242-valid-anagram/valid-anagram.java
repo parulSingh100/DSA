1f950cbff6fff761a3700fabc58936d55b70a1c9
Parul Singh <singhparulsingh7890@gmail.com>
2026-08-25 17:20:47 +0530

    Time: 16 ms (21.04%) | Memory: 46.1 MB (50.40%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        for(int i = 0; i < t.length(); i++ ){
+            map1.put(t.charAt(i),map1.getOrDefault(t.charAt(i),0)+1);
+        }
+    return map.equals(map1);    
+    }
+    
+}
