dcae91cad9ffacbcf1a7576c6ddb0c5de7063606
Parul Singh <singhparulsingh7890@gmail.com>
2026-09-06 13:52:05 +0530

    Time: 27 ms (9.55%) | Memory: 47.4 MB (97.86%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -20,9 +20,9 @@ class Solution {
                 answer = mid;
                 right = mid - 1;    
             }
-            // else if(totalTime <h){
-            //     right = mid - 1;
-            // }
+            else if(totalTime <h){
+                right = mid - 1;
+            }
             else{
                 left = mid + 1;
             }
