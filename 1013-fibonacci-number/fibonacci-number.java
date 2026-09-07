6edb4464900624d836b587d38d58b5715a2fad93
Parul Singh <singhparulsingh7890@gmail.com>
2026-09-07 22:22:09 +0530

    Time: 10 ms (25.92%) | Memory: 42.4 MB (16.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,19 +1,11 @@
 class Solution {
-    int [] dp ;
-    public int fibo(int n){
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=0){
-            return dp[n];
-        }
-        int ans = fib(n -1) + fib(n - 2);
-        return ans;
-    }
     public int fib(int n) {
-        dp = new int[n + 1];
-        return fibo(n);
-
-        
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
     }
 }
